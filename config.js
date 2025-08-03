0220ba38b3f768bf32d7e2b9f029823dbd8939d5
riobotpair <dena74137@gmail.com>
2025-08-03 18:49:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "Ps4RhLQI#v1Y8DN-2R74qS5iVqPo96qv-Z0xUmk2x50OR7UAX13o",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/riobotpair/RIO/blob/main/images/Vmake1754198681084.png",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/riobotpair/RIO/main/images/Vmake1754198681084.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 RIO-MD Is Alive Now😍*",
 BOT_OWNER: '94720630290',  // Replace with the owner's phone number
 
